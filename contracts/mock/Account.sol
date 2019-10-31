e137257ab7ed9118919f146cc81b90f4d8746ce4
crazyrabbitLTC <dennison@dennisonbertram.com>
2019-10-31 17:20:42 -0400

    Add Create2 Execute Test

---
+++ contracts/mock/Account.sol
@@ -0,0 +1,24 @@
+pragma solidity >0.4.99 <0.6.0;
+
+contract Account {
+    address public owner;
+
+    event accountOwnerchanged(address oldOwner, address newOwner);
+    constructor(address payable _owner) public {
+        owner = _owner;
+    }
+
+    function setOwner(address _owner) public {
+        require(msg.sender == owner, "Message Sender is not Owner");
+        address oldOwner = owner;
+        owner = _owner;
+        emit accountOwnerchanged(oldOwner, _owner);
+    }
+
+    function destroy(address payable recipient) public {
+        require(msg.sender == owner, "Message Sender is not Owner");
+        selfdestruct(recipient);
+    }
+
+    function() external payable {}
+}
---
+++ test/contractCall.js
@@ -0,0 +1,75 @@
+// Import all required modules from openzeppelin-test-helpers
+const {
+  BN,
+  constants,
+  expectEvent,
+  expectRevert
+} = require('@openzeppelin/test-helpers');
+
+const { expect } = require("chai");
+
+const IdentityFactory = artifacts.require("IdentityFactory");
+const Identity = artifacts.require("Identity");
+const AccountContract = artifacts.require("Account");
+
+contract("Identity: Create2", ([sender, receiver, thirdperson, fourthperson]) => {
+  let identity = null;
+
+  // converts an int to uint256
+  function numberToUint256(value) {
+    const hex = value.toString(16);
+    return `0x${"0".repeat(64 - hex.length)}${hex}`;
+  }
+
+
+  beforeEach(async function() {
+
+    this.identityFactory = await IdentityFactory.new();
+
+    await this.identityFactory.initialize({
+      from: sender
+    });
+
+    const { logs } = await this.identityFactory.createIdentity(
+      sender,
+      "Hello Metadata",
+      { from: sender }
+    );
+
+    identityAddress = logs[0].args.identityAddress;
+    registeredOwner = logs[0].args.owner;
+
+    identity = await Identity.at(identityAddress);
+  });
+
+  it("Identity deployed a Standard  contract", async () => {
+    const { abi: accountAbi, bytecode: accountBytecode } = AccountContract;
+
+    const { logs } = await identity.execute(
+      1,
+      sender,
+      0,
+      accountBytecode,
+      0,
+      { from: sender }
+    );
+
+    expectEvent.inLogs(logs, "contractCreated", {
+      contractType: new BN(1)
+    });
+  });
+
+
+
+  it("Should not deploy a contract if the sender is not whitelisted", async () => {
+    const { abi: accountAbi, bytecode: accountBytecode } = AccountContract;
+    await expectRevert(identity.execute(
+      2,
+      sender,
+      0,
+      accountBytecode,
+      0,
+      { from: thirdperson }
+    ), "WhitelistAdminRole: caller does not have the WhitelistAdmin role.");
+  });
+});
